8686f61fdbe639d9539a3b69e4d403437f081ca3
Fariha <fariha1032004@gmail.com>
2025-12-13 03:04:53 +0600

    Remove unused function and update input/output values for Newton's Forward Difference

--- Numerical_Differentiation/Newton's_Backward_Difference/newton's_backward_difference.cpp
+++ Numerical_Differentiation/Newton's_Backward_Difference/newton's_backward_difference.cpp
@@ -1,14 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n)
-{
-    int f = 1;
-    for (int i = 2; i <= n; i++)
-        f *= i;
-    return f;
-}
-
 int main()
 {
     ifstream fin("input.txt");
